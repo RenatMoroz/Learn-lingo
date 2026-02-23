31565f7fa43e25a572ee9065452dbd4fc4d2dd48
Renat Moroz <bs4123555@gmail.com>
2026-02-23 20:32:45 +0100

    abra

--- services/serverConfig.ts
+++ services/serverConfig.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const nextApi = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:3001',
+  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:3000',
   withCredentials: true,
 });
