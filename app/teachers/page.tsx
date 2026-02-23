45bc675da21bd3bf1bd99c07668e526d16edd68f
Renat Moroz <bs4123555@gmail.com>
2026-02-23 17:50:55 +0100

    fix deploy

--- app/teachers/page.tsx
+++ app/teachers/page.tsx
@@ -1,10 +1,13 @@
+import { Suspense } from 'react';
 import Teachers from '@/components/Teachers/Teachers';
 import css from './Page.module.css';
 
 const Page = () => {
   return (
     <div className={css['page']}>
-      <Teachers />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Teachers />
+      </Suspense>
     </div>
   );
 };
