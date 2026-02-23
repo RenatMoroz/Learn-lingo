88dcb0c2531963f9fb7c4a9f6e814396f2426b3e
Renat Moroz <bs4123555@gmail.com>
2026-02-23 19:19:19 +0100

    add env and deploy

--- app/api/serverConfig.ts
+++ app/api/serverConfig.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const globalApi = axios.create({
-  baseURL: 'http://localhost:3001',
+  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
   withCredentials: true,
 });
