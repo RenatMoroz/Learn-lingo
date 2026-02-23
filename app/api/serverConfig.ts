0c5c426441420851279378e8b94d007b7f78dd05
Renat Moroz <bs4123555@gmail.com>
2026-02-23 22:28:26 +0100

    undo

--- app/api/serverConfig.ts
+++ app/api/serverConfig.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const globalApi = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
+  baseURL: 'http://localhost:3001',
   withCredentials: true,
 });
--- app/teachers/page.tsx
+++ app/teachers/page.tsx
@@ -1,13 +1,10 @@
-import { Suspense } from 'react';
 import Teachers from '@/components/Teachers/Teachers';
 import css from './Page.module.css';
 
 const Page = () => {
   return (
     <div className={css['page']}>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Teachers />
-      </Suspense>
+      <Teachers />
     </div>
   );
 };
