c71dc31a21a584f11427f5457eaeb97a5df5e3fa
Renat Moroz <bs4123555@gmail.com>
2026-02-26 16:35:16 +0100

    min fix -config

--- services/serverConfig.ts
+++ services/serverConfig.ts
@@ -1,6 +1,12 @@
 import axios from 'axios';
 
+const serverOrigin =
+  process.env.NEXT_PUBLIC_APP_URL ||
+  (process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : '') ||
+  'http://localhost:3000';
+
 export const nextApi = axios.create({
-  baseURL: '/api',
+  baseURL:
+    typeof window === 'undefined' ? `${serverOrigin}/api` : '/api',
   withCredentials: true,
 });
